08d2cf28162ad3ebf7bf47285165f06fc727607b
Frederik Goethals <frederik@frontaal-it.be>
2020-11-16 15:21:58 +0100

    observables geconfigureerd

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import {catchError, tap, map} from 'rxjs/operators';
 import { User } from './user';
 import { Note } from './note';
 
@@ -8,15 +9,26 @@ import { Note } from './note';
   providedIn: 'root'
 })
 export class ApiService {
-  
+
   constructor(private http: HttpClient) { }
   // API end-point URL
   private readonly URL: string = 'https://experienced-icy-canary.glitch.me';
   
 
-  public getUsers(){
-    return this.http.get<User[]>(`${this.URL}/users`);
+  public getUsers(): Observable<any> {
+    return this.http.get<User[]>(`${this.URL}/users`).pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+    );
+}
+
+ /* public getUsers(){
+    return Observable.create(o => {
+      o.next( this.http.get<User[]>(`${this.URL}/users`) );
+    });
   }
+  */
+
   public createUser(user: User){}
   public deleteUser(user: User){}
 
@@ -27,6 +39,18 @@ export class ApiService {
     return this.http.post(`${this.URL}/users/`,user);
   }
 
-  
+  private handleError(err: HttpErrorResponse) {
+ 
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+
+        errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+
+        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+}
 
 }
